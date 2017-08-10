0a34f28b4775278e563d95263afb1ca553993d1c
Alexey Malakhov <au.malakhov@gmail.com>
2017-08-10 17:21:39 +0300

    space step2

---
+++ task2515/Space.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by AMalakhov on 10.08.2017.
+ */
+public class Space {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ task2515/SpaceShip.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by AMalakhov on 10.08.2017.
+ */
+public class SpaceShip {
+}
