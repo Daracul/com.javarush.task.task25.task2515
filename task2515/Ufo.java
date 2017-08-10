9321c4bd87f23366f2a69503f33073c17ba04744
Alexey Malakhov <au.malakhov@gmail.com>
2017-08-10 18:00:47 +0300

    space step6

--- task2515/Ufo.java
+++ task2515/Ufo.java
@@ -4,4 +4,7 @@ package com.javarush.task.task25.task2515;
  * Created by AMalakhov on 10.08.2017.
  */
 public class Ufo extends BaseObject{
+    public Ufo(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
