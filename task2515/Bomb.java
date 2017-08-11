99d634ebcbde4b82a9b6563ee9367374dcf5f52d
Alexey Malakhov <au.malakhov@gmail.com>
2017-08-11 12:08:27 +0300

    space step11

--- task2515/Bomb.java
+++ task2515/Bomb.java
@@ -1,10 +1,26 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Created by AMalakhov on 10.08.2017.
+ * Класс для бомбы.
  */
-public class Bomb extends BaseObject{
-    public Bomb(double x, double y, double radius) {
-        super(x, y, radius);
+public class Bomb extends BaseObject {
+    public Bomb(double x, double y) {
+        super(x, y, 1);
+    }
+
+    /**
+     * Отрисовываем себя на холсте.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'B');
+    }
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
+        y++;
     }
 }
